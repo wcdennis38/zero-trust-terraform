7d4d08474b1588bd34b000ee4a22124c41ae9463
wcdennis38 <myaws6000@gmail.com>
2026-06-24 21:38:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
-
-# REQUIRED for CSRF protection
-app.config["SECRET_KEY"] = "change-this-to-a-secure-random-value"
-
-csrf = CSRFProtect(app)
-
-@app.route("/")
-def home():
-    return "DevSecOps Pipeline Running in AWS!"
